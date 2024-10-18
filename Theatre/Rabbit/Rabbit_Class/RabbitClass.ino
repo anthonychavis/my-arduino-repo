06259b2979ccde45d28ec536d4e989e30e971fd0
Anthony Chavis <86731866+anthonychavis@users.noreply.github.com>
2024-10-18 15:22:32 -0400

    class works, but needs work

---
+++ Theatre/Rabbit/Rabbit_Class/RabbitClass.ino
@@ -0,0 +1,69 @@
+/*
+NOTE:
+    - CPx refers to pins of the Circuit Playground Express
+    - QT refers to the QT PY Pico
+    - might have to change pin value for other boards
+*/
+
+
+#include "Rabbit_Res.hpp"
+
+        // pass by reference ?? Passing the dependency by reference ensures that you're working with the same instance of the object throughout the lifetime of MyClass. ??
+        // should these be instantiated here ??
+            // how are they removed from the stack if here ??
+Servo myServo;
+Rabbit fluffyCute(myServo, true, 20, 160);  // explicit constructor call
+    // delete the objects ?? in destructor ??
+
+//// put your setup code here, to run once:
+void setup() {
+    // testing
+    Serial.begin(9600);
+    delay(1000);
+
+    // CPx; needs work:
+    if(issa_CPx) {
+        // if button-controlled led for quick functioning test
+        pinMode(boardLedPin, OUTPUT);  // for board-controlled LED
+        pinMode(ledBtnPin, INPUT_PULLDOWN);  // makes default status of the D4 btn = 0 (as opposed to truthy)
+        // board controlled switch (not between power & board)
+        pinMode(switchPin, INPUT_PULLUP);  // resistor needed for CPx onboard switch; 
+    }
+
+    // for mag connector/continuity
+    pinMode(contPin, INPUT_PULLUP);  // uses onboard resistor; 
+
+// HOW TO CREATE RABBIT FOR GLOBAL ACCESS, BUT ATTACH PIN HERE ?? !!
+
+    delay(1000);
+}
+
+//// put your main code here, to run repeatedly:
+void loop() {
+    // CPx; needs work:
+    if(issa_CPx) {
+        // (quick functioning test)
+        digitalWrite(boardLedPin, digitalRead(ledBtnPin));
+        // for board-controlled switch (not between power & board)
+        switchOn = digitalRead(switchPin);
+        if(!switchOn) {
+            delay(1000);
+            return;
+        }
+    }
+
+    if(fluffyCute.servoAttached()) {
+        curMillis = millis();
+        discontinuous = digitalRead(contPin);  // HIGH & True == 1; (0V input read at pin) LOW & False == 0
+        if(curMillis - timer > delayMS) {
+            !discontinuous ? fluffyCute.contFunc() : fluffyCute.discontFunc();
+        }
+    } else {
+        delay(500);
+        return;
+    }
+
+    if(!fluffyCute.twitchState()) {
+        fluffyCute.servoDetach();
+    }
+}
---
+++ Theatre/Rabbit/Rabbit_Class/RabbitClass_Res.hpp
@@ -0,0 +1,178 @@
+/*
+NOTE:
+    - CPx refers to pins of the Circuit Playground Express
+    - QT refers to the QT PY Pico
+    - might have to change pin value for other boards
+    - functions are not hoisted in this file
+    - servo power input - 5v
+*/
+
+#ifndef RABBITCLASS_RES_H
+
+#define RABBITCLASS_RES_H
+
+#if defined(ARDUINO_ARCH_ESP32)
+    #include <ESP32Servo.h>
+#else
+    #include <Servo.h>
+#endif
+
+
+//// VARS
+
+// Servo myServo;
+
+// this conditional needs much improvement !!
+#if defined(__SAMD21G18A__)  // CPx
+    #define issa_CPx true
+    // board-controlled LED for quick functionin test
+    #define ledBtnPin 4  // btn A - CPx
+    #define boardLedPin 13  // D13 - CPx
+    // board controlled switch (not between power & board)
+    bool switchOn;
+    #define switchPin 7  // D7 - CPx
+    // for servo
+    #define servoPin 10  // A3 == D10 - CPx
+    // for mag connector/continuity
+    #define contPin 1  // A7 == D1 - CPx;
+#else
+    #define issa_CPx false
+/*
+* for QT PY ESP32 Pico (until more specific, if possible);
+* change these pin values as needed for other boards
+*/
+    // for servo
+    #define servoPin 26  // A0 == D26 - QT (unless using wifi?) ??; pwm/~;
+    // for mag connector/continuity
+    #define contPin 15  // A3 == D15 - QT  (unless using wifi?) ??; digital input
+#endif
+
+// time
+unsigned long curMillis, timer = 0;
+uint16_t delayMS;
+
+// for mag connector
+bool discontinuous;
+
+
+//// CLASSES 
+
+class Rabbit {
+    Servo& servo;
+
+    // alive
+    uint8_t minAng, maxAng;  // test higher minAng & lower maxAng first to check how much the gears amplify the angles !!
+    uint8_t middleAng, prevAng, newAng;
+    const double vel = 60.0 / 250;  // degrees/millisecs
+
+    // dead
+    const uint16_t initTwitchVal = 100;  // min = 0; max = 65536
+    uint16_t twitch = initTwitchVal;
+    bool decapped = false, feetAtBigAng;
+    const float initAccel = 1.1;
+    float accel = initAccel;
+
+    // METHODS
+
+    uint8_t getMidAng() {
+        return (maxAng - minAng + 1) / 2;  // truncated
+    }  // might need a neutralAng depending on how it fxns w/model
+
+    // return angle difference
+    uint8_t angDiffFunc() {
+        int16_t angDiff = newAng - prevAng;
+        return angDiff >= 0 ? angDiff : abs(angDiff);  // don't calc w/in abs()
+    }
+
+    // set new timer val & write new angle
+    void newAngTimer() {
+        servo.write(newAng);
+        timer = curMillis;
+    }
+
+    // reset if accidental death
+    void easterBunny() {
+        twitch = initTwitchVal;
+        decapped = false;
+        accel = initAccel;
+        prevAng = newAng;
+    }
+
+public:
+        // Passing the dependency by reference ensures that you're working with the same instance of the object throughout the lifetime of the object. ??
+            // destructor not needed in this case b/c this class does not "own" the dependency; aka it was instantiated elsewhere
+                // how to remove from stack if instantiated in global scope ??
+    // use "explicit" to prevent implicit conversions ??
+    
+    // test higher minAng & lower maxAng first to check how much the gears amplify the angles !!
+    explicit Rabbit(Servo& aServo, bool feetTowardsHighAng, uint8_t lowAng = 10, uint8_t highAng = 170) : servo(aServo), feetAtBigAng(feetTowardsHighAng), minAng(lowAng), maxAng(highAng) {
+        // if(lowAng is >= 0 && highAng <= 180 && highAng > lowAng) {
+            middleAng = getMidAng();
+                servo.attach(servoPin);
+                servo.write(middleAng);
+                prevAng = middleAng;
+        // }
+    }
+
+    // alive; used while continuity through mag connector (or other method of continuity)
+    void contFunc() {
+        if(twitch != initTwitchVal) {
+            easterBunny();
+        }
+        newAng = random(minAng, maxAng);
+        delayMS = angDiffFunc() / vel + 1;
+        prevAng = newAng;
+        newAngTimer();
+    }
+
+    // dead; used while NO continuity through mag connector (or other method of continuity); return to middle angle
+    void discontFunc() {
+        // rev depending on motor orientation w/model
+            // use feetAtBigAng for automatic switch
+        if(!decapped) {  
+            // could add code for if position greater than 3/4 at start of this block !!
+            delay(50);
+            newAng = middleAng * 7 / 4;  // change to 1/4 if oriented in other direction; imagery = should curl up before loosening
+            // newAng = middleAng * (feetAtBigAng ? 7 : 1) / 4;  // check if this syntax works !!
+                Serial.print("newAng when decapped < 1: ");
+                Serial.println(newAng);
+            delayMS = angDiffFunc() / vel + 251;
+            decapped = true;
+        } else if(newAng == middleAng) {
+            // this block extends the accidental death allowance - see initTwitchVal
+            twitch--;
+            delayMS = 300;
+            timer = curMillis;
+            return;
+        } else {
+            // use feetAtBigAng to alter this !!
+            if(middleAng < newAng && accel < middleAng) {
+                newAng -= accel;
+                accel *= accel;
+            } else {
+                newAng = middleAng;
+            }
+            delayMS = 65;
+                Serial.print("newAng during drop: ");
+                Serial.println(newAng);
+                Serial.println(accel);
+        }
+        newAngTimer();
+    }
+
+    // GETTERS
+
+    bool servoAttached() {
+        return servo.attached();
+    }
+
+    uint8_t twitchState() {
+        return twitch;
+    }
+
+    void servoDetach() {
+        servo.detach();
+    }
+};
+
+#endif
